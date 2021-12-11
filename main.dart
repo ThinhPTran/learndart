b0be141a9f7d052b22334a188ed7c6cd6017fe75
Thinh P. Tran <trphthinh@gmail.com>
2021-12-11 16:54:00 +0700

    test list

--- main.dart
+++ main.dart
@@ -3,6 +3,10 @@ void main() {
   test_params(5, np1 : 5);
   test_params(5, np2 : 10);
 
+  var myList = ["apple" , "banana", "pear"];
+
+  myList.forEach((item) { print("${myList.indexOf(item)} : $item") ; });
+
 }
 
 test_params(p1 , {np1 = 25, np2 }) {
